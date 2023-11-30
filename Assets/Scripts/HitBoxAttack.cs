a0913b7ae295e0d9b05a63d38c6219e01c4392db
Matti Gencher <meg.g@live.com>
2023-11-30 15:30:39 -0500

    Make player damage what it hits

--- Assets/Scripts/HitBoxAttack.cs
+++ Assets/Scripts/HitBoxAttack.cs
@@ -7,22 +7,15 @@ public class HitBoxAttack : MonoBehaviour {
     public int comboNum;
     [SerializeField] private float[] damage = new float[3] { 10, 10, 15 };
 
-    private Health health; // Add reference to Health script
-
     private void OnEnable() {
         canAttack = true;
-
-        // Initialize health reference
-        health = GetComponentInParent<Health>();
     }
 
     private void OnTriggerEnter(Collider collider) {
         if (canAttack) {
-            Debug.Log("hit");
 
-            // TODO: Replace this with the actual damage value
             float damageAmount = damage[comboNum];
-            health.TakeDamage(damageAmount);
+            collider.gameObject.GetComponent<Health>().TakeDamage(damageAmount);
         }
         canAttack = false;
     }
