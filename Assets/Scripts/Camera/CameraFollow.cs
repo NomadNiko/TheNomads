e2e44e6cfdbeea827f70dc52ab36e1776e1dc5ac
Oblivion0173 <113329952+Oblivion0173@users.noreply.github.com>
2023-12-01 09:04:50 -0700

    added follow camera

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -1,18 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject player;       // Reference to the player GameObject
+    public float smoothing = 5f;    // Smoothing factor for camera movement
+
+    private Vector3 offset;         // Offset distance between the player and camera
+
+    // Use this for initialization
     void Start()
     {
-        
+        // Calculate the initial offset.
+        offset = transform.position - player.transform.position;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        // Create a postion for the camera to move towards
+        Vector3 targetCamPos = player.transform.position + offset;
+
+        // Smoothly interpolate between the camera's current position and it's target position
+        transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
     }
 }
